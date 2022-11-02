0948b7453c09a31e66d5a3871912e9c871a79dfe
sophie <soph_lew@hotmail.co.uk>
2022-11-02 14:56:13 +0000

    added red font to 'time' in Clock Class

--- Intro/Clock.java
+++ Intro/Clock.java
@@ -2,11 +2,14 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
 
 class Clock {
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_RESET = "\u001B[0m";
+
   public static void main(String[] args) {
     LocalDateTime dateTime = LocalDateTime.now();
     DateTimeFormatter formatTo = DateTimeFormatter.ofPattern("HH::mm");
     String formattedDate = dateTime.format(formatTo);
 
-    System.out.printf("The current time is %s \n", formattedDate);
+    System.out.printf("The current " + ANSI_RED + "time" + ANSI_RESET + " is %s \n", formattedDate);
   }
 }
