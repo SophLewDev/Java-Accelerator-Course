3323e4b31cea8fa0096f6066c72387124d9bbdd2
sophie <soph_lew@hotmail.co.uk>
2022-11-03 16:26:54 +0000

    refactored pricedisplay

--- Intro/pricedisplay/app/src/test/java/pricedisplay/PriceDisplayTest.java
+++ Intro/pricedisplay/app/src/test/java/pricedisplay/PriceDisplayTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 public class PriceDisplayTest {
   @Test
   public void displayingNameAndPrice() {
-    PriceDisplay display = new PriceDisplay();
     assertEquals("The price for item: Apple is 2", String.valueOf("The price for item: Apple is 2"));
   }
 }
