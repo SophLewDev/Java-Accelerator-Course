b49129352dda77a5cffe23f72cf4a95fa1be0d9f
sophie <soph_lew@hotmail.co.uk>
2022-11-03 16:06:46 +0000

    moved Calculator directory in Intro

--- Intro/PriceDisplay.java
+++ Intro/PriceDisplay.java
@@ -4,4 +4,8 @@ class PriceDisplay {
     Double price = 15.99;
     System.out.printf("The price for the item %s is %.2f", itemName, price);
   }
+
+  Integer getPriceLabel(String itemName, int price) {
+    System.out.printf("The price for item: %s is %i", itemName, price)
+  }
 }
