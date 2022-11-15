818262f9375357888a531469de37d0d9d1386a62
sophie <soph_lew@hotmail.co.uk>
2022-11-15 21:02:19 +0000

    modified and added contructor, and added extra test

---
+++ Intro/word-guessing-game/app/src/main/java/word/guessing/game/Game.java
@@ -0,0 +1,16 @@
+package word.guessing.game;
+
+import java.lang.*;
+
+public class Game {
+
+    public Game(String word) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+    public String getWordToGuess(String word) {
+        return word;
+    }
+}
---
+++ Intro/word-guessing-game/app/src/test/java/word/guessing/game/GameTest.java
@@ -0,0 +1,19 @@
+package word.guessing.game;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GameTest {
+    @Test public void testGetsWordToGuess1() {
+        Game game = new Game("B_____");
+        String word = "B_____";
+        assertEquals(game.getWordToGuess(word), word);
+    }
+
+    @Test public void testGetsWordToGuess2() {
+        Game game = new Game("Makers");
+        String word = "Makers";
+        assertEquals(game.getWordToGuess(word), word);
+    }
+}
